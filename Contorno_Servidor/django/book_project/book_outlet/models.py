c3c42767fb9c1e3d1f817b0cbbdd7d688e7d1a70
Chris-vd <a24christianvd@iessanclemente.net>
2025-12-01 10:18:25 +0100

    cs bookAuth

--- Contorno_Servidor/django/book_project/book_outlet/models.py
+++ Contorno_Servidor/django/book_project/book_outlet/models.py
@@ -3,10 +3,17 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False) #We are setting a default value
 
     def __str__(self):
--- Contorno_Servidor/django/book_project/book_outlet/views.py
+++ Contorno_Servidor/django/book_project/book_outlet/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Author
 from django.db.models import Q
 
 # Create your views here.
 def home(request):
     highest = Book.objects.order_by("-rating")[:3]
     query = Book.objects.filter(Q(title__icontains="Potter"), Q(Q(is_bestselling=True) | Q(rating__gte=3)))
-    jkr = Book.objects.filter(author="J.K. Rowling", is_bestselling=True)
+    jkr = Book.objects.filter(author=1, is_bestselling=True)
 
-    return render(request, "outlet/home.html", {"highest":highest, "query":query, "jkr":jkr})
+    rowling = Book.objects.filter(author__last_name = "Rowling")
+    rowling = Author.objects.get(last_name="Rowling").book_set.all()
+
+    return render(request, "outlet/home.html", {"highest":highest, "query":query, "jkr":jkr, "rowling":rowling})
